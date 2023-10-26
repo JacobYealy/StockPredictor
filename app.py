e2feccef6f9d4cdb0f4048d56f698dc6d6904f2e
YealyJ <jy0441@engr.ship.edu>
2023-10-26 11:20:16 -0400

    Added contact and about sections

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import json
-
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect, url_for
 from lstm_model import generate_plot_data
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Add form submission utils.
+@app.route('/submit_contact', methods=['POST'])
+def submit_contact():
+    # Handle form data and do something (e.g., store in the database, send an email, etc.)
+    return redirect(url_for('home'))  # Could set up email.js for simplicity
 
 @app.route('/get_plot_data')
 def get_plot_data_endpoint():
